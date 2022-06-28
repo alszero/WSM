3e724b181d7ad9b4d19a73db3319eeaac9c964a9
alszero <alszero05@gmail.com>
2022-06-28 15:48:55 +0900

    html,css,js addNow()

--- 20220607_food court/js/main.js
+++ 20220607_food court/js/main.js
@@ -17,6 +17,7 @@ const toggleMenu = (toggleId, navListId) => {
 
 }
 toggleMenu('nav-toggle', 'nav-list');
+
 // function say(){
 //     console.log('hello world');
 // } 
@@ -26,3 +27,36 @@ toggleMenu('nav-toggle', 'nav-list');
 // const say = () => console.log('hello world3');
 // say();
 
+const addNow = (homeCardId) => {
+    // html -> js
+    const homeCard = document.getElementById(homeCardId);
+    
+    // 시간 -> index번째 식사가 선택되어야하는지 결정
+    // 조식 끝 : 8시
+    // 중식 끝 : 1시
+    // 석식 끝 : 5시50분
+    const now = new Date();
+    let hour = 16; // 시 now.getHours()
+    let minute = 20; // 분 now.getMinutes()
+    console.log(hour, minute);
+    index = 0;
+
+    minute = hour * 60 + minute;
+
+    console.log(index);
+    
+    if(minute >= 17* 60 + 50){ //1070
+        index = 0;
+    }else if( 13*60 <= minute){
+        index = 2;
+    }else if(8*60 <= minute){
+        index = 1;
+    } else{
+        index = 0;
+    }
+
+    // homeCard에서 index번째 card에 now 클래스 추가
+    let card = homeCard.getElementsByClassName('card')[index];
+    card.classList.add('now');
+}
+addNow('home-card');
